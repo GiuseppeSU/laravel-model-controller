3362c92e4289a98ec06bf5373982dcd1ae576ea2
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-11 17:42:49 +0200

    Fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,22 @@
 
 </head>
 
-<body>
-    <h1 class="text-center">Elenco Film</h1>
-    <div class="container">
+<body class="my_container">
+    <h1 class="text-center mt-5 mb-5 text-light">PEPFLIX</h1>
+    <div class="container w-50 ">
         <div class="row">
             @foreach ($movies as $movie)
-                <div class="col-3">
-                    <li>{{ $movie->id }} - {{ $movie->title }} - {{ $movie->original_title }} -
-                        {{ $movie->nationality }} - {{ $movie->date }} - {{ $movie->vote }}
-                    </li>
+                <div class="col-4">
+                    <div class="card  m-3 ">
+                        <img src="https://vignette2.wikia.nocookie.net/logopedia/images/b/b2/NetflixIcon2016.jpg/revision/latest/scale-to-width-down/2000?cb=20160620223003"
+                            class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <p class="card-text">{{ $movie->title }} </p>
+                            <p class="card-text">{{ $movie->nationality }}</p>
+                            <p class="card-text">{{ $movie->date }}</p>
+                            <p class="card-text">{{ $movie->vote }}</p>
+                        </div>
+                    </div>
 
                 </div>
             @endforeach
